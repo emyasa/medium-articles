e4bcd9a21d8d065a3b87bb1a325867a406d6e643
Emmanuel Yasa <emyasa.emmanuel.yasa@gmail.com>
2022-01-23 21:17:28 +0800

    removes unused imports

--- spring-boot-oauth/authorization-server/src/main/java/com/emyasa/config/WebSecurityConfig.java
+++ spring-boot-oauth/authorization-server/src/main/java/com/emyasa/config/WebSecurityConfig.java
@@ -6,11 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
