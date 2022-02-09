6bae32e979214a2079c042edfbf8e7d98a58ea99
Emmanuel Yasa <emyasa.emmanuel.yasa@gmail.com>
2022-02-09 21:14:53 +0800

    recreates the schema on server for testing purposes

--- spring-boot-oauth/authorization-server/src/main/resources/schema.sql
+++ spring-boot-oauth/authorization-server/src/main/resources/schema.sql
@@ -1,3 +1,21 @@
+DROP TABLE IF EXISTS oauth2_registered_client;
+CREATE TABLE IF NOT EXISTS oauth2_registered_client (
+    id varchar(100) NOT NULL,
+    client_id varchar(100) NOT NULL,
+    client_id_issued_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    client_secret varchar(200) DEFAULT NULL,
+    client_secret_expires_at timestamp DEFAULT NULL,
+    client_name varchar(200) NOT NULL,
+    client_authentication_methods varchar(1000) NOT NULL,
+    authorization_grant_types varchar(1000) NOT NULL,
+    redirect_uris varchar(1000) DEFAULT NULL,
+    scopes varchar(1000) NOT NULL,
+    client_settings varchar(2000) NOT NULL,
+    token_settings varchar(2000) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS oauth2_authorization;
 CREATE TABLE IF NOT EXISTS oauth2_authorization (
     id varchar(100) NOT NULL,
     registered_client_id varchar(100) NOT NULL,
@@ -26,25 +44,10 @@ CREATE TABLE IF NOT EXISTS oauth2_authorization (
     PRIMARY KEY (id)
 );
 
+DROP TABLE IF EXISTS oauth2_authorization_consent;
 CREATE TABLE IF NOT EXISTS oauth2_authorization_consent (
     registered_client_id varchar(100) NOT NULL,
     principal_name varchar(200) NOT NULL,
     authorities varchar(1000) NOT NULL,
     PRIMARY KEY (registered_client_id, principal_name)
-);
-
-CREATE TABLE IF NOT EXISTS oauth2_registered_client (
-    id varchar(100) NOT NULL,
-    client_id varchar(100) NOT NULL,
-    client_id_issued_at timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    client_secret varchar(200) DEFAULT NULL,
-    client_secret_expires_at timestamp DEFAULT NULL,
-    client_name varchar(200) NOT NULL,
-    client_authentication_methods varchar(1000) NOT NULL,
-    authorization_grant_types varchar(1000) NOT NULL,
-    redirect_uris varchar(1000) DEFAULT NULL,
-    scopes varchar(1000) NOT NULL,
-    client_settings varchar(2000) NOT NULL,
-    token_settings varchar(2000) NOT NULL,
-    PRIMARY KEY (id)
 );
