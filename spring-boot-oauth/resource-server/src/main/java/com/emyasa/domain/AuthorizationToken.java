74c31ed4ca10f56209e2b7c80ccc28df9f3727af
Emmanuel Yasa <emyasa.emmanuel.yasa@gmail.com>
2022-02-12 03:03:23 +0800

    aligns AuthorizationToken domain

--- spring-boot-oauth/resource-server/src/main/java/com/emyasa/domain/AuthorizationToken.java
+++ spring-boot-oauth/resource-server/src/main/java/com/emyasa/domain/AuthorizationToken.java
@@ -1,18 +1,21 @@
 package com.emyasa.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "authorization_token")
+@Table(name = "oauth2_authorization")
 public class AuthorizationToken {
 
     @Id
     private String id;
 
+    @Column(name = "access_token_value")
     private String token;
 
+    @Column(name = "access_token_type")
     private String tokenType;
 
     protected AuthorizationToken() {}
