d845cbe5356467572eb8f9f75ea42016959afb84
Emmanuel Yasa <emyasa.emmanuel.yasa@gmail.com>
2022-08-05 00:08:09 +0800

    creates bare-minumum clojure project

---
+++ clojure-introduction/project.clj
@@ -0,0 +1,4 @@
+(defproject clojure-introduction "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :main clojure-introduction.core)
---
+++ clojure-introduction/src/clojure_introduction/core.clj
@@ -0,0 +1,4 @@
+(ns clojure-introduction.core
+  (:gen-class))
+
+(defn -main [])
