b7292d72b931d9acd6c90a017d321e5d7c81187d
Emmanuel Yasa <emyasa.emmanuel.yasa@gmail.com>
2022-08-21 15:12:08 +0800

    creates a webserver and redis component

--- clojure-introduction/src/clojure_introduction/system.clj
+++ clojure-introduction/src/clojure_introduction/system.clj
@@ -1,8 +1,10 @@
 (ns clojure-introduction.system
   (:require [com.stuartsierra.component :as component]
-            [clojure-introduction.webserver :as webserver]))
+            [clojure-introduction.webserver :as webserver]
+            [clojure-introduction.redis :as redis]))
 
 (defn system [system-config]
   (component/system-map
    :config system-config
-   :web-server (component/using (webserver/web-server) [:config])))
+   :web-server (component/using (webserver/make) [:config])
+   :redis (component/using (redis/make) [:config])))
--- clojure-introduction/src/clojure_introduction/webserver.clj
+++ clojure-introduction/src/clojure_introduction/webserver.clj
@@ -1,21 +1,52 @@
 (ns clojure-introduction.webserver
   (:require [clojure.tools.logging :as log]
             [com.stuartsierra.component :as component]
-            [ring.adapter.jetty :refer [run-jetty]]))
+            [ring.adapter.jetty :refer [run-jetty]])
+  (:import
+   java.lang.management.ManagementFactory
+   org.eclipse.jetty.jmx.MBeanContainer
+   [org.eclipse.jetty.server ConnectorStatistics Server]
+   org.eclipse.jetty.server.handler.StatisticsHandler))
 
-(defrecord WebServer []
+(defrecord WebServer [port])
+
+(defn init [config]
+  (if-let [server-config (:server config)]
+    (map->WebServer {:port (:port server-config)
+                     :enabled true})
+    (map->WebServer {:enabled false})))
+
+(defn- jetty-conf
+  [^Server server]
+  (let [mb-container    (MBeanContainer. (ManagementFactory/getPlatformMBeanServer))
+        default-handler (.getHandler server)
+        stats-handler   (StatisticsHandler.)]
+    (.addEventListener server mb-container)
+    (.addBean server mb-container)
+    (ConnectorStatistics/addToAllConnectors server)
+
+    (.setHandler stats-handler default-handler)
+    (.setHandler server stats-handler)
+    (.addBean server stats-handler)
+
+    (.setStopTimeout server 60000)
+    (.setStopAtShutdown server true)))
+
+(defrecord WebServerComponent [configurator]
   component/Lifecycle
   (start [this]
-         (let [port (get-in this [:config :server :port])]
-           (log/info "## Starting web server on port:" port)
-           (assoc this :http-server
-                  (run-jetty (fn [_] {:status 200}) {:port port}))))
+         (let [config (:config this)
+               webserver (init config)
+               port (:port webserver)]
+           (when (:enabled webserver)
+             (log/info "## Starting web server on port:" port)
+             (assoc this :http-server
+                    (run-jetty (fn [_] {:status 200}) (merge {:configurator configurator :join? false} {:port port}))))))
   (stop [this]
         (log/info "## Shutting down web server")
-        (if-let [server (:htt-server this)]
+        (if-let [server (:http-server this)]
           (.stop server)
           (log/warn "Web server hasn't been running"))
         (dissoc this :http-server)))
 
-(defn web-server [] 
-  (->WebServer))
+(defn make [] (->WebServerComponent {:configurator jetty-conf }))
